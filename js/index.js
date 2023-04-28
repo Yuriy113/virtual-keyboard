912c3a27871453fa2b715b108902d82b4fd53533
Yuriy Ulanov <1859697@gmail.com>
2023-04-28 23:19:08 +0300

    feat: service buttons behavior was implemented

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import keys from './Keys.js';
+
 document.body.style.backgroundColor = '#12ffef';
 
 const container = document.createElement('div');
@@ -24,48 +26,15 @@ pageLanguage.innerText = 'en';
 pageLanguage.classList.add('lang');
 container.appendChild(pageLanguage);
 
+const shortcutHint = document.createElement('p');
+shortcutHint.innerText = 'To switch the language, use the ctrl + alt keys';
+shortcutHint.classList.add('hint');
+container.appendChild(shortcutHint);
+
 const languages = ['en', 'ru'];
 const states = ['normal', 'shifted'];
 let currentLang = false;
 let currentState = false;
-
-const keys = [
-  // row1
-  { en: { normal: '`', shifted: '~' }, ru: { normal: 'ё', shifted: 'Ё' }, code: 'Backquote' },
-  { en: { normal: '1', shifted: '!' }, ru: { normal: '1', shifted: '!' }, code: 'Digit1' },
-  { en: { normal: '2', shifted: '@' }, ru: { normal: '2', shifted: '"' }, code: 'Digit2' },
-  { en: { normal: '3', shifted: '#' }, ru: { normal: '3', shifted: '№' }, code: 'Digit3' },
-  { en: { normal: '4', shifted: '$' }, ru: { normal: '4', shifted: ';' }, code: 'Digit4' },
-  { en: { normal: '5', shifted: '%' }, ru: { normal: '5', shifted: '%' }, code: 'Digit5' },
-  { en: { normal: '6', shifted: '^' }, ru: { normal: '6', shifted: ':' }, code: 'Digit6' },
-  { en: { normal: '7', shifted: '&' }, ru: { normal: '7', shifted: '?' }, code: 'Digit7' },
-  { en: { normal: '8', shifted: '*' }, ru: { normal: '8', shifted: '*' }, code: 'Digit8' },
-  { en: { normal: '9', shifted: '(' }, ru: { normal: '9', shifted: '(' }, code: 'Digit9' },
-  { en: { normal: '0', shifted: ')' }, ru: { normal: '0', shifted: ')' }, code: 'Digit0' },
-  { en: { normal: '-', shifted: '_' }, ru: { normal: '-', shifted: '_' }, code: 'Minus' },
-  { en: { normal: '=', shifted: '+' }, ru: { normal: '=', shifted: '+' }, code: 'Equal' },
-  {
-    en: { normal: 'Backspace', shifted: 'Backspace' },
-    ru: { normal: 'Backspace', shifted: 'Backspace' },
-    code: 'Backspace',
-  },
-  // row 2
-  { en: { normal: 'Tab', shifted: 'Tab' }, ru: { normal: 'Tab', shifted: 'Tab' }, code: 'Tab' },
-  { en: { normal: 'q', shifted: 'Q' }, ru: { normal: 'й', shifted: 'Й' }, code: 'KeyQ' },
-  { en: { normal: 'w', shifted: 'W' }, ru: { normal: 'ц', shifted: 'Ц' }, code: 'KeyW' },
-  { en: { normal: 'e', shifted: 'E' }, ru: { normal: 'у', shifted: 'У' }, code: 'KeyE' },
-  { en: { normal: 'r', shifted: 'R' }, ru: { normal: 'к', shifted: 'К' }, code: 'KeyR' },
-  { en: { normal: 't', shifted: 'T' }, ru: { normal: 'е', shifted: 'Е' }, code: 'KeyT' },
-  { en: { normal: 'y', shifted: 'Y' }, ru: { normal: 'н', shifted: 'Н' }, code: 'KeyY' },
-  { en: { normal: 'u', shifted: 'U' }, ru: { normal: 'г', shifted: 'Г' }, code: 'KeyU' },
-  { en: { normal: 'i', shifted: 'I' }, ru: { normal: 'ш', shifted: 'Ш' }, code: 'KeyI' },
-  { en: { normal: 'o', shifted: 'O' }, ru: { normal: 'щ', shifted: 'Щ' }, code: 'KeyO' },
-  { en: { normal: 'p', shifted: 'P' }, ru: { normal: 'з', shifted: 'З' }, code: 'KeyP' },
-  { en: { normal: '[', shifted: '{' }, ru: { normal: 'х', shifted: 'Х' }, code: 'BracketLeft' },
-  { en: { normal: ']', shifted: '}' }, ru: { normal: 'ъ', shifted: 'Ъ' }, code: 'BracketRight' },
-  { en: { normal: '\\', shifted: '|' }, ru: { normal: '\\', shifted: '/' }, code: 'Backslash' },
-  { en: { normal: 'del', shifted: 'del' }, ru: { normal: 'del', shifted: 'del' }, code: 'Delete' },
-];
 
 const removePrev = () => {
   let caret = textarea.selectionStart;
@@ -96,42 +65,62 @@ const removeNext = () => {
 };
 
 const createKeyboardKeys = (keys, lang, shift) => {
-  for (let value of keys) {
-    const key = document.createElement('button');
-    key.classList.add('keyboard__key');
-    key.innerText = value[lang][shift];
+  for (let key of keys) {
+    const button = document.createElement('button');
+    button.classList.add('keyboard__key');
+    button.classList.add(`${key.code}`);
+    button.innerText = key[lang][shift];
 
-    key.addEventListener('click', () => {
-      if (key.innerHTML === 'Backspace') {
+    button.addEventListener('click', () => {
+      if (key.code === 'Backspace') {
         removePrev();
         return;
       }
-
-      if (key.innerHTML === 'del') {
+      if (key.code === 'Delete') {
         removeNext();
         return;
       }
+      if (key.code === 'Tab') {
+        textarea.setRangeText(`    `, textarea.selectionStart, textarea.selectionEnd, 'end');
+        return;
+      }
+      if (key.code === 'ShiftLeft' || key.code === 'ShiftRight') {
+        // TODO: implement virtual shift behavior
+      }
 
-      textarea.value += key.innerText;
+      textarea.value += button.innerText;
       textarea.focus();
     });
-    keyboard.appendChild(key);
+    keyboard.appendChild(button);
   }
 };
 
 createKeyboardKeys(keys, languages[0], states[0]);
+
 document.addEventListener('click', () => textarea.focus());
 textarea.focus();
 
 document.addEventListener('keydown', (e) => {
-  let isArrow =
+  let buttons = document.querySelectorAll('.keyboard__key');
+
+  if (e.repeat) {
+    e.preventDefault();
+    return;
+  }
+
+  const isServiceKey =
     e.code === 'ArrowUp' ||
     e.code === 'ArrowDown' ||
     e.code === 'ArrowLeft' ||
-    e.code === 'ArrowRight';
-
-  if (e.repeat) return;
-  if (!isArrow) e.preventDefault();
+    e.code === 'ArrowRight' ||
+    e.code === 'Enter' ||
+    e.code === 'ShiftLeft' ||
+    e.code === 'ShiftRight' ||
+    e.code === 'ControlLeft' ||
+    e.code === 'ControlRight' ||
+    e.code === 'AltLeft' ||
+    e.code === 'AltRight' ||
+    e.code === 'Space';
 
   let lang = languages[+currentLang];
   let shift = states[+e.shiftKey];
@@ -139,10 +128,24 @@ document.addEventListener('keydown', (e) => {
     currentLang = !currentLang;
   }
 
-  let buttons = document.querySelectorAll('.keyboard__key');
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].innerText = keys[i][lang][shift];
     if (keys[i].code === e.code) {
+      buttons[i].classList.add('pressed');
+
+      if (!isServiceKey) {
+        e.preventDefault();
+        textarea.focus();
+      } else {
+        for (let key of keys) {
+          if (e.code === key.code) {
+            console.log(e.target);
+          }
+        }
+        console.log(e);
+        return;
+      }
+
       if (e.code === 'Backspace') {
         removePrev();
         return;
@@ -151,8 +154,11 @@ document.addEventListener('keydown', (e) => {
         removeNext();
         return;
       }
+      if (e.code === 'Tab') {
+        textarea.setRangeText(`    `, textarea.selectionStart, textarea.selectionEnd, 'end');
+        return;
+      }
 
-      buttons[i].classList.add('pressed');
       textarea.value += buttons[i].innerHTML;
     }
   }
